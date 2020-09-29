d5bd024239e912fcc014c82d92d8bcbf64d0c16f
LandonWeiss <39284079+LandonWeiss@users.noreply.github.com>
2020-09-28 23:15:26 -0600

    encabezadoStructs
    Este archivo contiene la definición de los structs del proyecto.

---
+++ structs.h
@@ -0,0 +1,58 @@
+
+struct Marca {
+	char *descripcion;
+} typedef Marca;
+
+struct Modelo {
+	char *descripcion;
+	int anio;
+	Marca *marca;
+} typedef Modelo;
+
+struct Avion {
+	char *matricula;
+	int anio;
+	Modelo *modelo;
+} typedef Avion;
+
+struct TipoAsiento {
+	char *tipo;
+	float precio;
+} typedef TipoAsiento;
+
+struct Asiento {
+	char *nombre;
+	char fila;
+	int estaOcupado;
+	TipoAsiento *tipo;
+} typedef Asiento;
+
+struct Vuelo {
+	char *origen;
+	char *destino;
+	char *fechaSalida;
+	char *fechaLlegada;
+	Avion *avion;
+	Asiento **asientos;
+} typedef Vuelo;
+
+struct UsuarioOperativo {
+	char *nombreUsuario;
+	char *contrasenia;
+	Avion **aviones;
+} typedef UsuarioOperativo;
+
+struct UsuarioGeneral {
+	char *pasaporte;
+	char *nombre;
+	char *apellido1;
+	char *apellido2;
+	char *fechaNacimiento;
+	char sexo;
+} typedef UsuarioGeneral;
+
+struct Reservacion {
+	char *fecha;
+	UsuarioGeneral **usuarios;
+	Asiento **asientos;
+} typedef Reservacion;
