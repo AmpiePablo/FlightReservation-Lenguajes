2733a542c87e32fd5303608e34b731584ab3901b
LandonWeiss <39284079+LandonWeiss@users.noreply.github.com>
2020-10-04 23:24:11 -0600

    feat-comentarioGetInput
    Se agregó la documentación interna de la funcion GetInput().

--- get_input.h
+++ get_input.h
@@ -1,6 +1,24 @@
 #ifndef GET_INPUT_H
 #define GET_INPUT_H
 
+/*
+ * GetInput
+ *
+ * Entradas:
+ *		Una secuencia de caracteres digitada por el usuario
+ *		por medio de la consola.
+ *
+ * Salidas:
+ *		La secuencia de caracteres digitada por el usuario
+ *		en forma de un char* delimitado por '\0'
+ *		(retorno de función)
+ *
+ * Objetivo:
+ *		Almacenar todos los caracteres que el usuario
+ *		quiera digitar por consola, sin limitación de
+ *		cantidad de caracteres ni caracteres de espacio,
+ *		y luego delimitarlo con '\0'
+ */
 char* GetInput() {
 	
 	unsigned int size = INIT_SIZE;
